ad70906dbd67c878c0dc1a578573468584b8de0d
NULLCT <cockatiel.u10@gmail.com>
2020-10-18 00:42:27 +0900

    change all

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <string>
+using std::string;
+
+class Human{
+  //TODO:
+};
+
+class Me : public Human{
+public:
+  int age = 15;
+  string country = "Japan";
+  string contact = "cockatiel.u10@gmail.com";
+  string favorite_languages[2] = {"C++", "BrainF**k"};
+
+private:
+  //Released with over 256 intimacy
+};
